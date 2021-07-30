9194903c40bc7eeb0218ad2d93d424a775db146d
Ilie Babcenco <ilie.babcenco@gmail.com>
2021-07-30 16:37:19 +0300

    Finish lesson 5 switch remove filter method to main in controller

--- src/main/java/com/example/sweater/controller/MainController.java
+++ src/main/java/com/example/sweater/controller/MainController.java
@@ -27,10 +27,17 @@ public class MainController {
     }
 
     @GetMapping("main")
-    public String main(Model model) {
+    public String main(@RequestParam(required = false) String filter, Model model) {
 
+        Iterable<Message> messages;
+        if (filter != null && !filter.isEmpty()) {
+            messages = messageRepo.findByTag(filter);
+        } else {
+            messages = messageRepo.findAll();
 
-        model.addAttribute("messages", messageRepo.findAll());
+        }
+        model.addAttribute("messages", messages);
+        model.addAttribute("filter", filter);
         return "main";
     }
 
@@ -42,17 +49,6 @@ public class MainController {
             Model model) {
         messageRepo.save(new Message(text, tag, user));
         model.addAttribute("messages", messageRepo.findAll());
-        return "main";
-    }
-
-    @PostMapping("filter")
-    public String filter (@RequestParam String filter, Model model) {
-        if (filter != null && !filter.isEmpty()) {
-            model.addAttribute("messages", messageRepo.findByTag(filter));
-        } else {
-            model.addAttribute("messages", messageRepo.findAll());
-        }
-
         return "main";
     }
 
