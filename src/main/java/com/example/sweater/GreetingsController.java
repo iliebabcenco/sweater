79a47c4438b9811f7c7702a5c0bd63484dd4a570
Ilie Babcenco <ilie.babcenco@gmail.com>
2021-07-30 12:44:14 +0300

    Finish lesson 2

--- src/main/java/com/example/sweater/GreetingsController.java
+++ src/main/java/com/example/sweater/GreetingsController.java
@@ -1,12 +1,23 @@
 package com.example.sweater;
 
+import com.example.sweater.domain.Message;
+import com.example.sweater.repository.MessageRepo;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class GreetingsController {
+
+    private final MessageRepo messageRepo;
+
+    @Autowired
+    public GreetingsController(MessageRepo messageRepo) {
+        this.messageRepo = messageRepo;
+    }
 
     @GetMapping("/greeting")
     public String greeting(
@@ -18,7 +29,26 @@ public class GreetingsController {
 
     @GetMapping
     public String main(Model model) {
-        model.addAttribute("some", "Buna ziua copii!");
+
+        model.addAttribute("messages", messageRepo.findAll());
+        return "main";
+    }
+
+    @PostMapping
+    public String add(@RequestParam String text, @RequestParam String tag, Model model) {
+        messageRepo.save(new Message(text, tag));
+        model.addAttribute("messages", messageRepo.findAll());
+        return "main";
+    }
+
+    @PostMapping("/filter")
+    public String filter (@RequestParam String filter, Model model) {
+        if (filter != null && !filter.isEmpty()) {
+            model.addAttribute("messages", messageRepo.findByTag(filter));
+        } else {
+            model.addAttribute("messages", messageRepo.findAll());
+        }
+
         return "main";
     }
 
---
+++ src/main/java/com/example/sweater/repository/MessageRepo.java
@@ -0,0 +1,10 @@
+package com.example.sweater.repository;
+
+import com.example.sweater.domain.Message;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
+
+public interface MessageRepo extends CrudRepository<Message, Integer> {
+    List<Message> findByTag(String tag);
+}
